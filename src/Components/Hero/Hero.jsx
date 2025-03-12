a1ca188a178b1282392ada28f8ae0ddf041ce04c
ThePowerty <w.infanzon.98@gmail.com>
2025-03-12 22:18:45 +0100

    Add download resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.png'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import CV from "../../assets/CV-FSD-2025.pdf";
 
 export const Hero = () => {
   return (
-    <div id='hero' className='hero'>
+    <div id="hero" className="hero">
       <img src={profile_img} alt="" />
-      <h1><span>I'm Williams,</span> full-stack developer.</h1>
-      <p>Mi nombre es Williams y soy desarrollador Full-Stack de Madrid, España con experiencia desarrollando con Java, Python, React y Angular entre otros.</p>
+      <h1>
+        <span>I'm Williams,</span> full-stack developer.
+      </h1>
+      <p>
+        Mi nombre es Williams y soy desarrollador Full-Stack de Madrid, España
+        con experiencia desarrollando con Java, Python, React y Angular entre
+        otros.
+      </p>
       <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Contacta conmigo</AnchorLink></div>
-        <div className="hero-resume">Curriculum</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Contacta conmigo
+          </AnchorLink>
+        </div>
+        <div className="hero-resume">
+          <a href={CV} download="CV-FSD-2025">
+            Curriculum
+          </a>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
