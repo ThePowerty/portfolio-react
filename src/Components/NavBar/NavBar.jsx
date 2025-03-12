8027c40eb14a70f35d43ce03f1dfc56b1584d8f5
Williams <w.infanzon.98@gmail.com>
2025-03-12 13:24:39 +0100

    Make responsive web

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './NavBar.css'
 import logo from '../../assets/logo.jpg'
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 export const NavBar = () => {
 
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right="0";
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right="-350px";
+  }
+
   return (
     <div className='navbar'>
-      <img src={logo} alt="" />
-      <ul className="nav-menu">
-        <li><AnchorLink className='anchor-link' href='#home'><p>Home</p></AnchorLink></li>
+      <img className='logo' src={logo} alt="" />
+      <img className='nav-mob-open' src={menu_open} alt="" onClick={openMenu}/>
+      <ul ref={menuRef} className="nav-menu">
+        <img src={menu_close} alt="" className="nav-mob-close" onClick={closeMenu} />
+        <li><AnchorLink className='anchor-link' href='#hero'><p>Home</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About me</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#projects'><p>Portfolio</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p>Contact</p></AnchorLink></li>
