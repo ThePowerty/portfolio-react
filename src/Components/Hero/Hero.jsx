daa34eb5c6b4d3211979e8a64f9a9ce100dd41d6
ThePowerty <w.infanzon.98@gmail.com>
2025-03-13 02:40:34 +0100

    update

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { Link } from 'react-router-dom'
 
 export const Hero = () => {
+
   return (
     <div id='hero' className='hero'>
       <img src={profile_img} alt="" />
@@ -12,9 +12,7 @@ export const Hero = () => {
       <p>Mi nombre es Williams y soy desarrollador Full-Stack de Madrid, España con experiencia desarrollando con Java, Python, React y Angular entre otros.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Contacta conmigo</AnchorLink></div>
-        <Link to="#/resume" target='_blank' className='hero-link'>
-          <div className="hero-resume">Curriculum</div>
-        </Link>
+        <div onClick={() => {window.open('/portfolio-react/resume', '_blank')}} className="hero-resume">Curriculum</div>
       </div>
     </div>
   )
--- src/Routers/Router.tsx
+++ src/Routers/Router.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter , HashRouter, Route, Routes } from 'react-router-dom';
-import Home from '../App.jsx';
-import Resume from '../Components/Resume/Resume.jsx';
+import React from "react";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import Home from "../App.jsx";
+import Resume from "../Components/Resume/Resume.jsx";
 
 export const Router = () => {
   return (
-    <HashRouter basename="/portfolio-react">
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/resume" element={<Resume/>} />
-        </Routes>
-    </HashRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/portfolio-react/" element={<Home />} />
+        <Route path="/portfolio-react/resume" element={<Resume />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
