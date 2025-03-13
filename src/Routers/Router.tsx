0de943c92112269913be910b262a9084bdbe748e
ThePowerty <w.infanzon.98@gmail.com>
2025-03-13 01:02:15 +0100

    update pfd

--- src/Routers/Router.tsx
+++ src/Routers/Router.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter , Route, Routes } from 'react-router-dom';
-import Home from '../App.jsx'; 
-import { Resume } from '../Components'; 
+import Home from '../App.jsx';
+import { Resume } from '../Components';
 
 export const Router = () => {
   return (
