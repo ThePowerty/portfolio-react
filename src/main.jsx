1eac9c680c05a9a4cf7101cc27ca2f0c08d2cf40
ThePowerty <w.infanzon.98@gmail.com>
2025-03-13 02:16:43 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Router } from "./Routers";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router />
+    <HashRouter>
+      <Router />
+    </HashRouter>
   </StrictMode>
 );
