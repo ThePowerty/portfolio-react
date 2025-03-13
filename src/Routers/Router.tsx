b33698d528dcafad6968c79d19955528296567d0
ThePowerty <w.infanzon.98@gmail.com>
2025-03-13 02:21:30 +0100

    update

--- src/Routers/Router.tsx
+++ src/Routers/Router.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import { BrowserRouter , HashRouter, Route, Routes } from 'react-router-dom';
 import Home from '../App.jsx';
 import Resume from '../Components/Resume/Resume.jsx';
 
 export const Router = () => {
   return (
-    <BrowserRouter basename="/portfolio-react">
+    <HashRouter basename="/portfolio-react">
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/resume" element={<Resume/>} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Router } from "./Routers";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
       <Router />
-    </HashRouter>
   </StrictMode>
 );
